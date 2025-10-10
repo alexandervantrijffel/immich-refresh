faba6abc4b9ca6c157d07e50a90224b35763920c
Alexander van Trijffel <alexander@structura.ws>
2025-10-10 22:14:22 +0200

    fix: quotes for album names

--- crates/refresh-cli/src/execute.rs
+++ crates/refresh-cli/src/execute.rs
@@ -54,7 +54,7 @@ impl Executer {
 
     fn format_command(&self, args: &ExecuteArgs) -> String {
         format!(
-            r#"{} upload -H -r -c 24 -A {} "{}/*""#,
+            r#"{} upload -H -r -c 24 -A "{}" "{}/*""#,
             IMMICH_CLI_COMMAND, args.album_name, args.path
         )
     }
